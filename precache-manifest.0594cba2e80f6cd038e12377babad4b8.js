cb44e76c03d7f9aa46d99c41a7042b02cf55bb83
Kirill <kirill-pavlovskij@rambler.ru>
2019-12-09 18:28:49 +0800

    Updates

---
+++ precache-manifest.0594cba2e80f6cd038e12377babad4b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b59740fe3f52dcbfade86929098039",
+    "url": "/chart-test/index.html"
+  },
+  {
+    "revision": "369b8b1547c1897b4cb5",
+    "url": "/chart-test/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5f3ba4a8d93c6cca81a9",
+    "url": "/chart-test/static/css/main.3bcaf9e5.chunk.css"
+  },
+  {
+    "revision": "369b8b1547c1897b4cb5",
+    "url": "/chart-test/static/js/2.c90d3ed6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chart-test/static/js/2.c90d3ed6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f3ba4a8d93c6cca81a9",
+    "url": "/chart-test/static/js/main.26cd419e.chunk.js"
+  },
+  {
+    "revision": "bc5ed696e8623214a4a9",
+    "url": "/chart-test/static/js/runtime-main.d9a82450.js"
+  }
+]);
